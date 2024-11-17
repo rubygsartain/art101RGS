f5a0d1fae569eb7e394c07cdec62cba1639fc99b
rubygsartain <rubygsartain@gmail.com>
2024-11-16 18:58:01 -0800

    lab11

---
+++ lab11/js/lab.js
@@ -0,0 +1,62 @@
+/*
+
+// index.js - Lab 10
+// Author: Ruby Grossman-Sartain
+// Date: 11/016/24
+
+*/
+
+
+
+
+// functions
+
+// sorts the characters of a string in alphabetical order
+
+function sortString(inputString) {
+return inputString.split('').sort().join('');
+}
+
+// event listener for button
+
+$("#submit").click(function() {
+// get input field value
+const userName = $("#user-name").val();
+
+// check for entered name
+
+if (userName) {
+    const userNameSorted = sortString(userName);
+
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>')
+
+} else {
+
+// select output div/append a new div with mod username
+
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
+}
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - Lab 8
+// index.js - Lab 12
 // Author: Ruby Grossman-Sartain
-// Date: 11/014/24
+// Date: 11/14/24
 
 // Constants
 
@@ -32,6 +32,17 @@ else if (mod == 4) {
     return "Very doubtful"
   }
  }
+
+ function checksum (str){
+  var hash = 0;
+  if (str.length == 0) return hash;
+  for (i = 0; i < str.length; i++) {
+      char = str.sortingTruth(i);
+      hash = ((hash<<5)-hash)+char;
+      hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
  
  var myButton = document.getElementById("button");
  myButton.addEventListener("click", function() {
