8559c75d67948f63587f9952101d7c2334343e85
rubygsartain <rubygsartain@gmail.com>
2024-11-23 22:03:58 -0800

    lab 12 fix

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -9,45 +9,39 @@
 // Functions
 
 
-function sortingTruth(str)
- {
-  len = str.length;
-  mod = len % 4;
-  if (mod ==0) {
-    return "Without a doubt"
-  }  
-  else if (mod == 1) {
-    return "It is certain"
+function sortingTruth(str) {
+  let len = str.length;
+  let mod = len % 6; // Change to 6 to match the number of responses
+  if (mod === 0) {
+      return "Without a doubt";
+  } else if (mod === 1) {
+      return "It is certain";
+  } else if (mod === 2) {
+      return "Better not tell you now";
+  } else if (mod === 3) {
+      return "Concentrate and ask again";
+  } else if (mod === 4) {
+      return "Don't count on it";
+  } else if (mod === 5) {
+      return "Very doubtful";
   }
-  else if (mod == 2) {
-    return "Better not tell you now"
-  }
-else if (mod == 3) {
-    return "Concentrate and ask again"
-  }
-else if (mod == 4) {
-    return "Don't count on it"
-  }
-  else if (mod == 5) {
-    return "Very doubtful"
-  }
- }
+}
 
- function checksum (str){
-  var hash = 0;
-  if (str.length == 0) return hash;
-  for (i = 0; i < str.length; i++) {
-      char = str.sortingTruth(i);
-      hash = ((hash<<5)-hash)+char;
+function checksum(str) {
+  let hash = 0;
+  if (str.length === 0) return hash;
+  for (let i = 0; i < str.length; i++) {
+      let char = str.charCodeAt(i); // Get the character code
+      hash = ((hash << 5) - hash) + char; // Hashing algorithm
       hash = hash & hash; // Convert to 32bit integer
   }
   return hash;
 }
- 
- var myButton = document.getElementById("button");
- myButton.addEventListener("click", function() {
-    var name = document.getElementById("input").value;
-    var answer = sortingTruth(input);
-    return newText = "<p> The universe holds: " + answer + "</p>";
-    document.getElementById("output").innerHTML = newText;
- })
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var answer = sortingTruth(name); // Use 'name' instead of 'input'
+  var newText = "<p>The universe holds: " + answer + "</p>"; // Create the output text
+  document.getElementById("output").innerHTML = newText; // Set the output
+});
