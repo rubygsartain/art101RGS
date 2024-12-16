dfc1e88636af60491ba03123c30267147215cb6a
rubygsartain <rubygsartain@gmail.com>
2024-12-15 23:29:09 -0800

    lab15fix

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,39 +1,35 @@
 // index.js - Lab 12
 // Author: Ruby Grossman-Sartain
-// Date: 11/25/24
+// Date: 12/15/24
 
-// Ensure jQuery is included in your HTML file
-// <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+$(document).ready(function() {
+    // Get the button element by its ID
+    const button = document.getElementById('Activate');
 
-// Get the button element by its ID
-const button = document.getElementById('activate');
-
-// Add a click event listener to the button
-button.addEventListener('click', function() {
-    // Using the core $.ajax() method
-    $.ajax({
-        // The URL for the request (from the API docs)
-        url: "https://yourapiendpoint.com/", // Replace with your actual API endpoint
-        // The data to send (will be converted to a query string)
-        data: { 
-            // Here is where any data required by the API goes (check the API docs)
-            id: 123,
-            api_key: "blahblahblah", // Replace with your actual API key
-        },
-        // Whether this is a POST or GET request
-        type: "GET",
-        // The type of data we expect back
-        dataType: "json",
-        // What do we do when the API call is successful
-        success: function(data) {
-            // Do stuff with the data
-            console.log(data);
-        },
-        // What we do if the API call fails
-        error: function(jqXHR, textStatus, errorThrown) { 
-            // Do stuff
-            console.log("Error:", textStatus, errorThrown);
-        }
+    // Add a click event listener to the button
+    button.addEventListener('click', function() {
+        // Using the core $.ajax() method
+        $.ajax({
+            // The URL for the request (from the API docs)
+            url: "https://api.nasa.gov/planetary/apod", // NASA API endpoint
+            // The data to send (will be converted to a query string)
+            data: {
+                api_key: "njxUNBBITfn7v9re7H7AMgwQcRn7b2dJ8oJF8ULh" // Include api_key here
+            },
+            // Whether this is a POST or GET request
+            type: "GET",
+            // The type of data we expect back
+            dataType: "json",
+            // What do we do when the API call is successful
+            success: function(data) {
+                console.log(data); // Log the data for debugging
+                $("#output").append("<h1>" + data.title + "</h1>"); // Use data.title for the title
+                $("#output").append("<img src='" + data.url + "' alt='NASA Image'>"); // Use data.url for the image
+            },
+            // What we do if the API call fails
+            error: function(jqXHR, textStatus, errorThrown) { 
+                console.log("Error:", textStatus, errorThrown);
+            }
+        });
     });
 });
-
